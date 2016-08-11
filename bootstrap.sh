b79baa69058601e4580b3908c3f60e3fa9d4d2e5
Pranay Sashank Pisupati <Pranay Sashank Pisupati>
2016-08-11 16:03:31 +0530

    install fastcgi

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,4 +11,4 @@ apt-get install -y python-software-properties
 LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 apt-get update
 
-apt-get install -y php7.0-common php7.0-fpm php7.0-mysql
+apt-get install -y php7.0-common libapache2-mod-fastcgi php7.0-fpm php7.0-mysql
